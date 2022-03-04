ade2111e377b7f399ba5a6e1df8dd7157b482b11
Jerrick Liu <jerrick.y.liu@gmail.com>
2022-03-04 13:29:34 -0600

    update script

--- vtracer/vtrace.py
+++ vtracer/vtrace.py
@@ -1,12 +1,32 @@
 import os
 import sys
 
-def vtrace(directory, save_dir, path_to_vtracer):
+def vtrace(directory, path_to_vtracer):
+
+    # Check if the directory exists
+    if not os.path.exists(directory):
+        print("The directory does not exist")
+        sys.exit(1)
+
+    # Check if the path to vtracer exists
+    if not os.path.exists(path_to_vtracer):
+        print("The path to vtracer does not exist")
+        sys.exit(1)
+
+    # Create a directory for the output
+    output_directory = directory + "_vectorized"
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
+
     for root, dirs, files in os.walk(directory):
+        # Make all dirs in the output directory
+        for d in dirs:
+            if not os.path.exists(output_directory + "/" + d):
+                os.makedirs(output_directory + "/" + d)
+
         for file in files:
             if file.endswith(".png") or file.endswith(".jpg"):
-                output = os.path.splitext(file)[0] + ".svg"
-                os.system(path_to_vtracer + " --input " + os.path.join(root, file) + " --output " + os.path.join(save_dir, output))
-
+                output_file = output_directory + root.replace(directory, "") + "/" + file.replace(".png", ".svg")
+                os.system(path_to_vtracer + " --input " + os.path.join(root, file) + " --output " + output_file)
 if __name__ == "__main__":
-    vtrace(sys.argv[1], sys.argv[2], sys.argv[3])
+    vtrace(sys.argv[1], sys.argv[2])
