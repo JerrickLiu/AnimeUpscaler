82fec954fe8755320faf86623b9d04627f428024
Jerrick Liu <jerrick.y.liu@gmail.com>
2022-04-27 00:34:42 -0500

    Add cain args

--- models/CAIN/config.py
+++ models/CAIN/config.py
@@ -16,6 +16,9 @@ data_arg = add_argument_group('Dataset')
 data_arg.add_argument('--dataset', type=str, default='vimeo90k')
 data_arg.add_argument('--num_frames', type=int, default=3)
 data_arg.add_argument('--data_root', type=str, default='data/vimeo_triplet')
+data_arg.add_argument('--orig_video_path', type=str, default=None)
+data_arg.add_argument('--save_video_path', type=str, default=None)
+data_arg.add_argument('--test_data_root', type=str, default='data/vimeo_triplet')
 data_arg.add_argument('--img_fmt', type=str, default='png')
 
 # Model
