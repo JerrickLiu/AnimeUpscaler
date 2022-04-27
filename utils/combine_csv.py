d5f55e13b6ca8dbb6ae602358e936ce21d530f0f
Jerrick Liu <jerrick.y.liu@gmail.com>
2022-04-27 00:34:15 -0500

    Add more utils

--- utils/combine_csv.py
+++ utils/combine_csv.py
@@ -2,26 +2,28 @@ import pandas as pd
 import glob
 import os
 
-files = "/home/jerrick/disk_mount/anime_data/small_extracted_frames/metadata/"
-
-# Get all csv files in the metadata folder and subfolders
-csv_files = glob.glob(os.path.join(files, "**/*scenes.csv"), recursive=True)
-
-
-# joining files with concat and read_csv
-dfs = []
-for f in csv_files:
-    df = pd.read_csv(f, header=1)
-    # Get number of rows
-    n = df.shape[0]
-    # Create a new column with the file name
-    file_name = f.split("/")[-1]
-    # Remove the scenes.csv from the file name
-    file_name = file_name.replace(".scenes.csv", "")
-
-    df['file_name'] = file_name
-    dfs.append(df)
+def combine_csv(csv_path):
+    # Get all csv files in the metadata folder and subfolders
+    csv_files = glob.glob(os.path.join(csv_path, "**/*scenes.csv"), recursive=True)
 
 
-df = pd.concat(dfs, ignore_index=True)
-# df.to_csv('/home/jerrick/disk_mount/anime_data/small_extracted_frames/metadata/all_scenes.csv', index=False)
+    # joining files with concat and read_csv
+    dfs = []
+    for f in csv_files:
+        df = pd.read_csv(f, header=1)
+        # Get number of rows
+        n = df.shape[0]
+        # Create a new column with the file name
+        file_name = f.split("/")[-1]
+        # Remove the scenes.csv from the file name
+        file_name = file_name.replace(".scenes.csv", "")
+
+        df['file_name'] = file_name
+        dfs.append(df)
+
+        # Remove the original csv file
+        os.remove(f)
+
+
+    df = pd.concat(dfs, ignore_index=True)
+    df.to_csv(csv_path + 'all_scenes.csv', index=False)
