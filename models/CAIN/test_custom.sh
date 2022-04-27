9616efc10b23c183ceafede720314cd515b426bc
Jerrick Liu <jerrick.y.liu@gmail.com>
2022-04-27 00:28:48 -0500

    Update cain testing script

--- models/CAIN/test_custom.sh
+++ models/CAIN/test_custom.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-CUDA_VISIBLE_DEVICES=0 python generate.py \
+CUDA_VISIBLE_DEVICES=1 python generate.py \
     --exp_name CAIN_fin \
     --dataset custom \
-    --data_root data/frame_seq \
+    --data_root /media/sahil/DL_5TB/MachineLearning/anime_playlist_downloads/fight_scenes_extracted_frames/extracted_frames/avatar_copy \
+    --orig_video_path /media/sahil/DL_5TB/MachineLearning/anime_playlist_downloads/fight_scenes/fight_scenes/avatar_original.webm \
+    --save_video_path /home/jerrick/AnimeUpscaler/videos/CAIN_avatar_OP_upscaled.mp4 \
     --img_fmt png \
     --batch_size 32 \
     --test_batch_size 16 \
