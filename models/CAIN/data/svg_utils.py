d2c07211bf1db3b71704cfcda667abe38a9943b9
Jerrick Liu <jerrick.y.liu@gmail.com>
2022-05-02 14:57:51 -0500

    Clean up files

--- models/CAIN/data/svg_utils.py
+++ models/CAIN/data/svg_utils.py
@@ -6,51 +6,8 @@ import os
 from tqdm import tqdm
 import torch
 import torch.nn as nn
-
-# file = sys.argv[1]
-
-# print('file', file)
-# dir = sys.argv[1]
-
-# # with open(file, 'rb') as f:
-    # # data = f.read()
-
-# # print(data)
-
-# for file in tqdm(os.listdir(dir)):
-    # if file.endswith(".svg"):
-        # # print(dir + file)
-        # df = pd.read_xml(dir + file)
-        # # print(df.head())
-        # z_all = []
-
-        # for i in range(len(df)):
-            # print(df.iloc[i]['transform'])
-            # path = df.iloc[i]['d']
-            # curves = path.split('C')
-            # per_curve = ([(curves[i].strip().split(' ')) for i in range(len(curves))])
-            # # if per_curve[0][0] != 'M0':
-                # # print("AHHHH")
-            # per_curve = per_curve[1:]
-            # per_curve[-1] = per_curve[-1][:-1]
-            # zs = 0
-            # for i in range(len(per_curve)):
-                # p = per_curve[i]
-                # if len(p) > 6:
-                    # zs += 1
-                    # per_curve[i] = p[:6]
-                # # for n in p:
-                    # # if n.isalpha():
-                        # # print(per_curve)
-                        # # print(n)
-                        # # zs += 1
-                        # # pass
-            # # z_all.append(zs)
-            # per_curve = np.array(per_curve)
-            # # print(per_curve.shape)
-            # # print(per_curve / len(curves))
-#         # print(np.unique(np.array(z_all), return_counts=True))
-
+import cv2
+from functools import reduce
 
 def load_segments(vector_file:str):
     """
@@ -84,21 +41,21 @@ def load_segments(vector_file:str):
         curves[-1] = curves[-1][:-1] # remove the last element, which is the Z
 
         # pruning out M and Z in the middle
-        for i in range(len(curves)):
-            p = curves[i]
+        for j in range(len(curves)):
+            p = curves[j]
             if len(p) > 6:
-                curves[i] = p[:6]
+                curves[j] = p[:6]
         # convert all string to float()
-        for i in range(len(curves)):
-            for j in range(len(curves[i])):
-                curves[i][j] = float(curves[i][j])
+        for k in range(len(curves)):
+            for j in range(len(curves[k])):
+                curves[k][j] = float(curves[k][j])
 
         # prepend previous curve's last point to the curve
-        for i in range(len(curves)):
-            if i == 0:
-                curves[i] = [0, 0] + curves[i]
+        for j in range(len(curves)):
+            if j == 0:
+                curves[j] = [0, 0] + curves[j]
             else:
-                curves[i] = curves[i-1][-2:] + curves[i]
+                curves[j] = curves[j-1][-2:] + curves[j]
 
         curves_np = np.array(curves)
         segments.append(curves_np)
@@ -113,17 +70,19 @@ def load_segments(vector_file:str):
     return segments, colors, transforms
 
 
-def pad_segments(segments):
+def post_process_svg_info(svg_info):
     """
-    Pads the segments to be of the same length.
+    Pads the svg_info to be all of same length. Also converts each element to a tensor.
 
     Args:
-        segments: List of numpy arrays of curves.
+        svg_info: a tuple of (segments, colors, transforms).
     Returns:
-        List of numpy arrays of padded curves.
+        A tuple of (segments, colors, transforms) that are padded to be all of same length and are tensors.
     """
 
     # Find max length of segments
+    segments, colors, transforms = svg_info
+
     max_len = 0
     for segment in segments:
         if segment.shape[0] > max_len:
@@ -131,5 +90,161 @@ def pad_segments(segments):
 
     # Pad segments to max length with -1    
     for i in range(len(segments)):
-        segments[i] = np.pad(segments[i], ((0, max_len - segments[i].shape[0]), (0, 0)), 'constant', constant_values=-1)
-    return segments
+        segments[i] = np.pad(segments[i], ((0, max_len - segments[i].shape[0]), (0, 0)), constant_values=-1)
+    
+    segments = torch.Tensor(segments)
+    colors = torch.Tensor(colors).unsqueeze(1).expand(-1, max_len, -1)
+    transforms = torch.Tensor(transforms).unsqueeze(1).expand(-1, max_len, -1)
+
+    return segments, colors, transforms
+
+
+def get_factors(n):    
+    return set(reduce(list.__add__, 
+                ([i, n//i] for i in range(1, int(n**0.5) + 1) if n % i == 0)))
+
+def k_box_clustering(segments, translations, k, return_seg_centroids=True):
+    """
+    Clusters the segments into k-boxes.
+
+    Args:
+        k: Number of clusters.
+        svg_info: a tuple of (segments, colors, transforms)
+        image: Pillow image of the original image.
+
+    Returns:
+        A list of k-boxes (each is a list of indices of segments).
+
+    """
+
+    # TODO: Don't hardcode this
+    width, height = 426, 240
+
+    aspect_ratio = width / height
+    factors = list(get_factors(k))
+
+    # Find two factors of k that are closest to the aspect ratio
+    min_factors = []
+    min_aspect_ratio = 9999
+    for i in range(len(factors)):
+        for j in range(i, len(factors)):
+            if abs(factors[i] / factors[j] - aspect_ratio) < min_aspect_ratio and factors[i] * factors[j] == k:
+                min_aspect_ratio = abs(factors[i] / factors[j] - aspect_ratio)
+                min_factors = [factors[i], factors[j]]
+
+
+    # Divide the image into k-boxes
+    k_boxes = []
+
+    if width > height:
+        min_factor_x = min_factors[1]
+        min_factor_y = min_factors[0]
+        x_ratio = width / min_factors[1]
+        y_ratio = height / min_factors[0]
+
+    else:
+        min_factor_x = min_factors[0]
+        min_factor_y = min_factors[1]
+        x_ratio = width / min_factors[0]
+        y_ratio = height / min_factors[1]
+
+    for i in range(min_factor_x):
+        for j in range(min_factor_y):
+            # Get the bounding box of the k-box
+            x_start = x_ratio * i
+            x_end = x_ratio * (i + 1)
+            y_start = y_ratio * j
+            y_end = y_ratio * (j + 1)
+
+            box = (x_start, y_start, x_end, y_end)
+            k_boxes.append(box)
+
+    segment_centroids = [weighted_centroid(segments[i], translations[i]) for i in range(len(segments))]
+    segment_centroids = np.array(segment_centroids)
+
+    print(segment_centroids.shape)
+
+    # Assign segments to k-boxes
+    # If segment centroid is within the bounding box, assign it to the k-box
+
+    clusters = [[] for i in range(k)]
+
+    for i in range(len(segment_centroids)):
+        segment_centroid = segment_centroids[i]
+        for j in range(len(k_boxes)):
+            box = k_boxes[j]
+
+            # Check if segment centroid is within the bounding box
+            if segment_centroid[0] >= box[0] and segment_centroid[0] <= box[2] and segment_centroid[1] >= box[1] and segment_centroid[1] <= box[3]:
+                clusters[j].append(i)
+                break
+
+    if return_seg_centroids:
+        return clusters, segment_centroids
+    return clusters
+
+def kmeans_centroids(segments, translations, color, k, return_seg_centroids=True):
+    """
+    K-means clustering of the centroids of the segments
+    
+    @param segments: list of numpy arrays of shape (n_i, 8)
+    @param translations: numpy array (n, 2). Holds the translations of the segments
+    @param color: color of the segments (n, 3)
+    @param k: number of clusters
+    @return: list of length k containing the indices of components in each cluster
+    """
+    s_centroids = [weighted_centroid(segments[i], translations[i]) for i in range(len(segments))]
+    s_centroids = np.array(s_centroids) # (#segments, 2)
+    print(s_centroids.shape)
+    # print(segment_centroids)
+
+    # append color to segment_centroids
+    segment_centroids = np.concatenate((s_centroids, np.array(color)/20), axis=1)
+    mins = np.min(segment_centroids, axis=0)
+    maxes = np.max(segment_centroids, axis=0)
+    centroids = np.random.rand(k, 5)
+    centroids = centroids * (maxes - mins) + mins
+
+    clusters = np.zeros(segment_centroids.shape[0], dtype=np.int)
+    for i in range(100):
+        # assign each point to the nearest centroid
+        for j in range(segment_centroids.shape[0]):
+            clusters[j] = np.argmin(np.linalg.norm(segment_centroids[j] - centroids, axis=1))
+        # print(clusters)
+        # update the centroids
+        for j in range(k):
+            if np.sum(clusters == j) == 0:
+                centroids[j] = np.random.rand(5)
+                # centroids[j, 0] = centroids[j, 0] * (x_max - x_min) + x_min
+                # centroids[j, 1] = centroids[j, 1] * (y_max - y_min) + y_min
+                centroids[j] = centroids[j] * (maxes - mins) + mins
+                continue
+            centroids[j] = segment_centroids[clusters == j].mean(axis=0)
+
+    sorted_clusters = [np.where(clusters == i)[0] for i in range(k)]
+    if return_seg_centroids:
+        return sorted_clusters, s_centroids
+    return sorted_clusters
+    
+def weighted_centroid(segment, translation):
+    """
+    Calculates a weighted centroid of a segment.
+    Weighting is done by looking at length of line segments for each point.
+
+    @params segment: numpy array of shape (n, 8)
+    @param translation: translation of the segment
+    @return: numpy array of shape (2)
+    """
+    centroid = np.zeros(2)
+    sum_weights = 0
+    for i in range(segment.shape[0]):
+        # print(segment[i, 0:2], segment[i, 6:8])
+        # weight = np.linalg.norm(segment[i][0] - segment[i][-1])
+        weight = np.linalg.norm(segment[i, 0:2] - segment[i, 6:8])
+        # weight = 1
+        centroid += (segment[i, 0:2] + segment[i, 6:8]) * weight
+        # print(centroid)
+        # print(weight)
+        sum_weights += weight * 2
+    centroid /= sum_weights
+    return centroid + translation
