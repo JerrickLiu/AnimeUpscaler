d6afdb5772eb2ab0adab4ba4373d6e5ee4ee81ae
Jerrick Liu <jerrickliu@hal-login2.ncsa.illinois.edu>
2022-05-04 21:03:40 -0500

    Reformat shell script

--- models/CAIN/run_main.sh
+++ models/CAIN/run_main.sh
@@ -1,1 +1,12 @@
-CUDA_VISIBLE_DEVICES=0 python main.py --exp_name VEC_TEST_INT --batch_size 4 --test_batch_size 1 --dataset anime_vectorized --model cain --max_epoch 200 --lr 0.0001 --data_root /media/sahil/DL_5TB/MachineLearning/anime_playlist_downloads/fight_scenes_extracted_frames --test_data_root /media/sahil/DL_5TB/MachineLearning/anime_playlist_downloads/test_fight_scenes --svg_dir /media/sahil/DL_5TB/MachineLearning/anime_playlist_downloads/fight_scenes_extracted_frames --test_svg_dir /media/sahil/DL_5TB/MachineLearning/anime_playlist_downloads/test_fight_scenes/
+python main.py \
+    --exp_name VEC \
+    --batch_size 4 \
+    --test_batch_size 1 \
+    --dataset anime_vectorized \
+    --model cain \
+    --max_epoch 200 \
+    --lr 0.0001 \
+    --data_root /media/sahil/DL_5TB/MachineLearning/anime_playlist_downloads/fight_scenes_extracted_frames \
+    --test_data_root /media/sahil/DL_5TB/MachineLearning/anime_playlist_downloads/test_fight_scenes \
+    --svg_dir /media/sahil/DL_5TB/MachineLearning/anime_playlist_downloads/fight_scenes_extracted_frames \
+    --test_svg_dir /media/sahil/DL_5TB/MachineLearning/anime_playlist_downloads/test_fight_scenes/
